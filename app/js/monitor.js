d565c383d7fb9f4e76314504c3a51850d3c5f7d7
Franco Martinez Carballo <franco.mcarballo@outlook.com>
2020-06-08 22:34:50 -0600

    first commi

---
+++ app/js/monitor.js
@@ -0,0 +1,93 @@
+const path = require('path');
+const osu = require('node-os-utils');
+const cpu = osu.cpu;
+const mem = osu.mem;
+const os = osu.os;
+
+let cpuOverload = 50;
+let alertFrequency = 5; // minutes
+
+// Run every  2 seconds
+setInterval(() => {
+  // CPU Usage
+  cpu.usage().then((info) => {
+    document.getElementById('cpu-usage').innerText = info + '%';
+
+    document.getElementById('cpu-progress').style.width = info + '%';
+
+    // Make progress bar red if overload
+    if (info >= cpuOverload) {
+      document.getElementById('cpu-progress').style.background = 'red';
+    } else {
+      document.getElementById('cpu-progress').style.background = '#30c88b';
+    }
+
+    // Check overload
+    if (info >= cpuOverload && runNotify(alertFrequency)) {
+      notifyUser({
+        title: 'CPU Overload',
+        body: `CPU is over ${cpuOverload}%`,
+        icon: path.join(__dirname, 'img', 'icon.png'),
+      });
+
+      localStorage.setItem('lastNotify', +new Date());
+    }
+  });
+
+  // CPU Free
+  cpu.free().then((info) => {
+    document.getElementById('cpu-free').innerText = info + '%';
+  });
+
+  // Uptime
+  document.getElementById('sys-uptime').innerText = secondsToDhms(os.uptime());
+}, 2000);
+
+// Set Model
+document.getElementById('cpu-model').innerText = cpu.model();
+
+// Computer Name
+document.getElementById('comp-name').innerText = os.hostname();
+
+// OS
+document.getElementById('os').innerText = `${os.type()} ${os.arch()}`;
+
+// Total Memory
+mem.info().then((info) => {
+  document.getElementById('mem-total').innerText = info.totalMemMb;
+});
+
+// show days, hours, mins, secs
+function secondsToDhms(seconds) {
+  seconds = +seconds;
+  const d = Math.floor(seconds / (3600 * 24));
+  const h = Math.floor((seconds % (3600 * 24)) / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+  const s = Math.floor(seconds % 60);
+
+  return `${d}d, ${h}h, ${m}m, ${s}s`;
+}
+
+// Send nofications
+function notifyUser(options) {
+  new Notification(options.title, options);
+}
+
+// check how much time has passed since notification
+function runNotify(frequency) {
+  if (localStorage.getItem('lastNotify') === null) {
+    // Store timestamp
+    localStorage.setItem('lastNotify', +new Date());
+    return true;
+  }
+  const notifyTime = new Date(parseInt(localStorage.getItem('lastNotify')));
+  const now = new Date();
+  const diffTime = Math.abs(now - notifyTime);
+  const minutesPassed = Math.ceil(diffTime / (1000 * 60));
+
+  if (minutesPassed > frequency) {
+    return true;
+  } else {
+    return false;
+  }
+}
---
+++ main.js
@@ -0,0 +1,86 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const log = require('electron-log');
+const Store = require('./Store');
+
+// Set env
+process.env.NODE_ENV = 'development';
+
+const isDev = process.env.NODE_ENV !== 'production' ? true : false;
+const isMac = process.platform === 'darwin' ? true : false;
+
+let mainWindow;
+
+// Init Store & Defaults
+const store = new Store({
+  configName: 'user-settings',
+  defaults: {
+    settings: {
+      cpuOverload: 80,
+      alertFrequency: 5,
+    },
+  },
+});
+
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    title: 'SysCPU',
+    width: isDev ? 800 : 355,
+    height: 600,
+    icon: './assets/icons/icon.png',
+    resizable: isDev ? true : false,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.loadFile('./app/index.html');
+}
+
+app.on('ready', () => {
+  createMainWindow();
+
+  mainWindow.webContents.on('dom-ready', () => {
+    mainWindow.webContents.send('settings:get', store.get('settings'));
+  });
+
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+});
+
+const menu = [
+  ...(isMac ? [{ role: 'appMenu' }] : []),
+  {
+    role: 'fileMenu',
+  },
+  ...(isDev
+    ? [
+        {
+          label: 'Developer',
+          submenu: [
+            { role: 'reload' },
+            { role: 'forcereload' },
+            { type: 'separator' },
+            { role: 'toggledevtools' },
+          ],
+        },
+      ]
+    : []),
+];
+
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
+
+app.allowRendererProcessReuse = true;
